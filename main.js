1ee8352e984b8e9830a157360c792d8f622fcebc
Byung-Jun <53166205+Byung-Jun@users.noreply.github.com>
2020-08-31 21:38:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,18 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
   var rtn = "";
-function templateHTML(title, list, body, control){
-
+function templateHTML(control, str, des){
+  if(control === 1){
+return `
+  <!doctype html>
+  <html>  
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+<img src="` + des + `" alt="OCR Image">
+  <p>URL Image에서 추출한 TEXT</p><h1>`+str+`</h1></body></html>`;
+  }else{    
       return `
   <!doctype html>
   <html>
@@ -20,15 +30,15 @@ function templateHTML(title, list, body, control){
             </p>
             <p> Example URL
             </p>
-            <p> https://raw.githubusercontent.com/Byung-Jun/ocr/master/dream.jpg
-            </p>
+            <p style="margin-bottom : 0;">https://raw.githubusercontent.com/Byung-Jun/ocr/master/dream.jpg</p>
             <p>
               <input type="submit">
             </p>
     </form>
   </body>
   </html>
-  `;
+  `;  
+  }
 }
 var app = http.createServer(function(request,response){
   var _url = request.url;
@@ -39,7 +49,7 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var template = templateHTML();
+          var template = templateHTML(0,'hello', 'node js');
           response.writeHead(200);
           response.end(template);
         });
@@ -53,6 +63,7 @@ var app = http.createServer(function(request,response){
           var post = qs.parse(body);
           var title = post.title;
           var description = post.description;
+          description = description.replace(/\s/gi, "");
           console.log(description);
   //image url
   var ocrUrl = 'https://9bae41e4b0354e8abbf7078e48f3b0b8.apigw.ntruss.com/custom/v1/3566/f683b34cc0de2187d754f445e6d467b812b1a53929842ec699673c812cf9ce95/general';
@@ -84,22 +95,9 @@ for(idx in body.images[0].fields){
   str += body.images[0].fields[idx].inferText + " ";
     //console.log(body.images[0].fields[idx]);
 };
-rtn = `
-  <!doctype html>
-  <html>  
-  <head>
-    <meta charset="utf-8">
-  </head>
-  <body>
-<img src="`;
-rtn += description;
-rtn += `" alt="OCR Image">
-  <p>입력한 URL Image에서 나온 TEXT</p><p>`;
-rtn += str;
-rtn += `  </p></body>
-  </html>`;
+rtn = templateHTML(1, str, description);
 console.log(rtn);
-    response.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
+    response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
     response.end(rtn);
 });
       });
